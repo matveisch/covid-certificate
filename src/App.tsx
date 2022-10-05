99a26d0ca25b74fb10cff8860074cbf3f1dc6b96
Matvei <matvesch@protonmail.com>
2022-10-05 18:35:44 +0300

    new date

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
+import engCert from './images/english-cert.png';
+import rusCert from './images/russian-cert.png';
+import logo from './images/logo_gosuslugi_na_sai_te_ritualmfcru.png';
+import engLang from './images/uk-circle-01.png';
+import rusLang from './images/rus-eng.png';
 import './App.css';
 
 function App() {
+    const [eng, setEng] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header>
+            <a href="https://www.gosuslugi.ru/"><img id="logo" src={logo} alt=""/></a>
+            <div id="lang" onClick={() => setEng(!eng)} style={{cursor: 'pointer'}}>
+                <img src={eng ? engLang : rusLang} alt="" style={eng ? {} : {width: '28px'}}/>
+                <h1>{eng ? 'ENG' : 'RUS'}</h1>
+            </div>
+        </header>
+        <main>
+            <img src={eng ? engCert : rusCert} alt=""/>
+            <div className="hr"></div>
+            <div id="data">
+                <div id="info">
+                    <h1>{eng ? 'V*** A*** D***' : 'В*** А*** Д***'}</h1>
+                    <h2>{eng ? 'Date of birth:' : 'Дата рождения:'} <span>04.12.2001</span></h2>
+                    <h2>{eng ? 'National passport:' : 'Паспорт:'} <span>36** ***858</span></h2>
+                </div>
+                <a id="button" href="https://www.gosuslugi.ru/">{eng ? 'Close' : 'Закрыть'}</a>
+            </div>
+        </main>
     </div>
   );
 }
